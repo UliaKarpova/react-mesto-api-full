09524f16fb44f72a1a2262fd58ce64d63fa44363
UliaKarpova <chelovek-e-95@mail.ru>
2022-09-01 14:40:14 +0300

    fix cors22

--- backend/src/middlewares/CORS.js
+++ backend/src/middlewares/CORS.js
@@ -6,8 +6,9 @@ const allowedCors = [
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports.CORS = (req, res, next) => {
-  console.log(req.headers.origin);
   const { origin } = req.headers;
+  console.log(origin);
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
